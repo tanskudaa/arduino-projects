f8f090fa4e0766347d60adcff06d8c51531ccd16
Taneli Hongisto <taneli.hongisto@protonmail.com>
2022-07-03 13:41:39 +0300

    .clang-format and formatting

--- thermo.ino
+++ thermo.ino
@@ -13,8 +13,7 @@
 */
 #include <LiquidCrystal.h>
 
-enum
-{
+enum {
     D7 = 9, // LCD data pins
     D6 = 8,
     D5 = 7,
@@ -30,21 +29,14 @@ enum
     TMP = A0, // Temperature sensor
 };
 
-const char CELSIUS_SYMBOL = 0b11011111; // From LCD datasheet
+const uint8_t CELSIUS_SYMBOL = 0b11011111; // From LCD datasheet
 
 int tmpRead;
 double tmpV;
 double temperature;
-char lcdState[2][16], newOutput[2][16] = {
-                          {0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0},
-                          {0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0}};
-int i, j;
+uint8_t lcdState[2][16],
+    newOutput[2][16] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 LiquidCrystal lcd(RS, E, D0, D1, D2, D3, D4, D5, D6, D7);
 
@@ -52,52 +44,40 @@ LiquidCrystal lcd(RS, E, D0, D1, D2, D3, D4, D5, D6, D7);
     Insert substring into string starting from index
     NOTE std::string would probably be more readable
 */
-void insertAt(char *string, char *substring, int startingIndex, int substringLength)
-{
-    for (i = 0; i < substringLength; i++)
-    {
+void insertToStringAt(uint8_t *string, uint8_t *substring, int startingIndex,
+                      int substringLength) {
+    for (int i = 0; i < substringLength; i++) {
         string[startingIndex + i] = substring[i];
     }
 }
 
 /*
-    Insert character into string at index
+    Insert uint8_tacter into string at index
 */
-void insertAt(char *string, char substring, int index)
-{
-    insertAt(string, &substring, index, 1);
+void insertToStringAt(uint8_t *string, uint8_t substring, int index) {
+    insertToStringAt(string, &substring, index, 1);
 }
 
 /*
-    Get single digit of number and convert to ASCII character
+    Get single digit of number and convert to ASCII uint8_tacter
 */
-char getDigitAsAscii(double number, double decimal)
-{
+uint8_t getDigitAsAscii(double number, double decimal) {
     return ((int)floor(number / decimal) % 10) + 48;
 }
 
 /*
-    Return ASCII NULL if zero, otherwise character
+    Return ASCII NULL if zero, otherwise uint8_tacter
 */
-char nonZeroOrNull(char digit)
-{
-    return digit == 48
-               ? 0
-               : digit;
-}
+uint8_t nonZeroOrNull(uint8_t digit) { return digit == 48 ? 0 : digit; }
 
 /*
     Update LCD screen
-    Only changed characters updated to reduce flicker
+    Only changed uint8_tacters updated to reduce flicker
 */
-void updateLCDOutput(LiquidCrystal lcd, char newOutput[2][16])
-{
-    for (j = 0; j < 2; j++)
-    {
-        for (i = 0; i < 15; i++)
-        {
-            if (lcdState[j][i] != newOutput[j][i])
-            {
+void updateLCDOutput(LiquidCrystal lcd, uint8_t newOutput[2][16]) {
+    for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < 15; i++) {
+            if (lcdState[j][i] != newOutput[j][i]) {
                 lcd.setCursor(i, j);
                 lcd.print(newOutput[j][i]);
                 delay(1);
@@ -106,17 +86,15 @@ void updateLCDOutput(LiquidCrystal lcd, char newOutput[2][16])
     }
 }
 
-void setup()
-{
+void setup() {
     Serial.begin(9600); // Initialize serial coms
     lcd.begin(16, 2);   // Initialize LCD screen
 
-    insertAt(newOutput[0], (char *)"Temp:", 2, 6);
+    insertToStringAt(newOutput[0], (uint8_t *)"Temp:", 2, 6);
     updateLCDOutput(lcd, newOutput);
 }
 
-void loop()
-{
+void loop() {
     /*
         "TMP36 [has] an output scale factor of 10 mV/°C. [...] The TMP36 is
         specified from –40°C to +125°C, provides a 750 mV output at 25°C".
@@ -130,12 +108,13 @@ void loop()
 
     Serial.println("temperature: " + String(temperature)); // debug
 
-    insertAt(newOutput[0], nonZeroOrNull(getDigitAsAscii(temperature, 10)), 8);
-    insertAt(newOutput[0], getDigitAsAscii(temperature, 1), 9);
-    insertAt(newOutput[0], '.', 10);
-    insertAt(newOutput[0], getDigitAsAscii(temperature, 0.1), 11);
-    insertAt(newOutput[0], CELSIUS_SYMBOL, 12);
-    insertAt(newOutput[0], 'C', 13);
+    insertToStringAt(newOutput[0],
+                     nonZeroOrNull(getDigitAsAscii(temperature, 10)), 8);
+    insertToStringAt(newOutput[0], getDigitAsAscii(temperature, 1), 9);
+    insertToStringAt(newOutput[0], '.', 10);
+    insertToStringAt(newOutput[0], getDigitAsAscii(temperature, 0.1), 11);
+    insertToStringAt(newOutput[0], CELSIUS_SYMBOL, 12);
+    insertToStringAt(newOutput[0], 'C', 13);
     updateLCDOutput(lcd, newOutput);
 
     delay(1500);
